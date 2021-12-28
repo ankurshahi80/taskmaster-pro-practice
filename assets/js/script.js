9e0c69eaf5df7d15c3d3d90f84b0e9cf1da31dc1
Ankur Shahi <shahiankur80@gmail.com>
2021-12-28 10:37:10 -0500

    drag task to dropzone to remove it

--- assets/js/script.js
+++ assets/js/script.js
@@ -236,3 +236,16 @@ $(".card .list-group").sortable({
   }
 });
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: 'touch',
+  drop: function(event,ui){
+    ui.draggable.remove();
+  },
+  over: function(event, ui){
+    console.log('over');
+  },
+  out: function(event, ui){
+    console.log('out');
+  }
+});
